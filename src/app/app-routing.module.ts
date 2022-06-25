1a88ffb6682a058c2e1883f3585f40a39d39efe1
eparrales <edwinparrales82@gmail.com>
2022-06-25 18:41:49 -0500

    Cambios para formularios del cliente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClienteComponent } from './cliente/cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cliente', component: ClienteComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
